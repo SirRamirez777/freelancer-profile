7b45184b49ee84011c8759370c74dac733b2eecb
Nhlakanipho Khoza 2 <nhlakanipho.khoza2@durban.gov.za>
2024-10-12 17:21:06 +0200

    Adding error handing

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -35,6 +35,7 @@ interface UserProfile {
 })
 export class UserProfileComponent implements OnInit {
   profile: UserProfile | undefined = undefined;
+  error: string | undefined = undefined;
 
   constructor(private http:HttpClient) {}
   
@@ -43,26 +44,10 @@ export class UserProfileComponent implements OnInit {
     this.http.get<UserProfile>('http://localhost:3001').subscribe(data => {
       console.log(data)
       this.profile = data
+    }, err => {
+      this.error = "Error here"
+      console.log(err)
     })
-    // this.profile = {
-    //   name: 'Nhlakanipho Romeo Khoza',
-    //   location: 'Durban, Umhlanga',
-    //   hourlyRate: 100,
-    //   bio: 'I will be showcasing some of my work and extra features and all the usual stuff you would expect. I hope you dig it!',
-    //   skills: ['Angular', 'TypeScript', 'CSS', 'JavaScript', 'HTML'],
-    //   portfolio: [
-    //     { title: 'Project 1', link: 'http://example.com', image: 'assets/project1.jpg' },
-    //     { title: 'Project 2', link: 'http://example.com', image: 'assets/project2.jpg' }
-    //   ],
-    //   socialMedia: [
-    //     { platform: 'LinkedIn', url: 'https://www.linkedin.com/in/nhlakanipho-romeo-khoza-779a60130/' },
-    //     { platform: 'GitHub', url: 'https://github.com/SirRamirez777' }
-    //   ],
-    //   contact: {
-    //     email: 'nhlakaniphoromeo95@gmail.com',
-    //     phone: '+27729965256'
-    //   }
-    // };
   }
 }
 
